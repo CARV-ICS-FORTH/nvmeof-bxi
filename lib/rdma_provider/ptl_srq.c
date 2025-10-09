ecb693a842077bcfc76abbb579bc5f2a6777646c
Giorgos Saloustros <gesalous@ics.forth.gr>
2025-10-09 16:17:54 +0300

    [feat] Allocate a PTE per ptl_srq

--- lib/rdma_provider/ptl_srq.c
+++ lib/rdma_provider/ptl_srq.c
@@ -3,6 +3,7 @@
 #include "ptl_log.h"
 #include "ptl_object_types.h"
 #include "ptl_pd.h"
+#include "spdk/util.h"
 
 static int ptl_post_srq_recv(struct ibv_srq *srq, struct ibv_recv_wr *recv_wr,
 			     struct ibv_recv_wr **bad_recv_wr)
@@ -11,7 +12,7 @@ static int ptl_post_srq_recv(struct ibv_srq *srq, struct ibv_recv_wr *recv_wr,
 	return 0;
 }
 
-struct ptl_srq *ptl_create_srq(struct ptl_pd *ptl_pd,
+struct ptl_srq *ptl_srq_create(struct ptl_pd *ptl_pd,
 			       struct ibv_srq_init_attr *srq_init_attr)
 {
 	struct ptl_srq * ptl_srq;
@@ -20,6 +21,21 @@ struct ptl_srq *ptl_create_srq(struct ptl_pd *ptl_pd,
 	struct ptl_context * ptl_cnxt = ptl_pd_get_cnxt(ptl_pd);
 	ptl_srq->fake_srq.context = ptl_cnxt_get_ibv_context(ptl_cnxt);
 	ptl_srq->fake_srq.context->ops.post_srq_recv = ptl_post_srq_recv;
-	SPDK_PTL_DEBUG("Created a PTL_SRQ...");
+	ptl_srq->pte_number = ptl_cnxt_allocate_pte(ptl_cnxt);
+	if (-1 == ptl_srq->pte_number) {
+		SPDK_PTL_FATAL("Out of PTEs sorry");
+	}
+	SPDK_PTL_DEBUG("Created a PTL_SRQ at PTE: [ %u ]", ptl_srq->pte_number);
 	return ptl_srq;
 }
+
+
+struct ptl_srq *ptl_srq_get_from_ibv_srq(struct ibv_srq *ibv_srq)
+{
+	struct ptl_srq *ptl_srq = SPDK_CONTAINEROF(ibv_srq, struct ptl_srq, fake_srq);
+	if (ptl_srq->obj_type != PTL_SRQ) {
+		SPDK_PTL_FATAL("Corrupted PTL_SRQ");
+	}
+	return ptl_srq;
+}
+
