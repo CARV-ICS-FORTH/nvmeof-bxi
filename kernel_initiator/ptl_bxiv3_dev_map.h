f79c7902c7b449bfaad6e93e010c711e4b716fbb
Giorgos Saloustros <gesalous@ics.forth.gr>
2025-12-23 17:55:31 +0200

    [kernel][refactor] Change max nicia num to 2 and formatted code.

--- kernel_initiator/ptl_bxiv3_dev_map.h
+++ kernel_initiator/ptl_bxiv3_dev_map.h
@@ -2,9 +2,9 @@
 #ifndef PTL_BXIV3_DEV_MAP_H
 #define PTL_BXIV3_DEV_MAP_H
 #include <linux/types.h>
-#define PTL_MAX_NICIA_NUM 16
+#define PTL_MAX_NICIA_NUM 2
 struct ptl_bxiv3_dev_map {
-  struct ptl_bxiv3_device *bxiv3_dev[PTL_MAX_NICIA_NUM];
-  u32 num_nicia;
+	struct ptl_bxiv3_device *bxiv3_dev[PTL_MAX_NICIA_NUM];
+	u32 num_nicia;
 };
 #endif
